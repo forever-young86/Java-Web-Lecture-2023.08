8be12a4c01099e5d3ea6e6bdd0fa95b92f1e3421
Younghwa Shin <gloryhwa@gmail.com>
2023-08-14 16:17:36 +0900

    0814 bootstrap

---
+++ demo/src/main/java/com/example/demo/BoostrapeController.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/bs")
+public class BoostrapeController {
+	@Value("${spring.servlet.multipart.location}") private String uploadDir;
+
+	@GetMapping("/login")
+	public String login() {
+		return "bs/login";
+	}
+	
+	@PostMapping("login1") //파라미터를 받는것 HttpServletRequest
+	@ResponseBody
+	public String login1(HttpServletRequest req) {
+		String email = req.getParameter("email");
+		String pswd = req.getParameter("pswd");
+		String remember = req.getParameter("remember"); //check면 on, 아니면 null
+		
+		return "<h2>email: " + email + ", pswd: "+ pswd + ", remember: "+ remember + "</h2>";
+	}
+
+	@PostMapping("login2")
+	@ResponseBody
+	public String login2(String email, String pswd, String remember) {
+		return "<h2>email: " + email + ", pswd: "+ pswd + ", remember: "+ remember + "</h2>";
+	}
+
+	@GetMapping("/register")
+	public String registerForm() {
+		return "bs/register";
+	}
+	
+	@PostMapping("/register")
+	@ResponseBody
+	public String registerProc(MultipartHttpServletRequest req) {
+		String id = req.getParameter("id");
+		String pswd = req.getParameter("pswd");
+		String pswd2 = req.getParameter("pswd2");
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		String gender = req.getParameter("gender");
+		String html = String.format("<h2>%s, %s, %s, %s, %s, %s</h2>", id, pswd, pswd2, name, email, gender);
+		
+		//파일읽기
+		MultipartFile filePart = req.getFile("profile");
+		if (filePart.getContentType().contains("image")) { //사진을 보냈음
+			String filename = filePart.getOriginalFilename();
+			html += "<h2>"+ filename + "</h2>"; 
+			String profilePath = uploadDir + "filename";
+			try {
+				filePart.transferTo(new File(profilePath));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+			
+		return html;
+	}
+}
